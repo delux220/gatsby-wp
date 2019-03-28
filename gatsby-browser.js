f6afcb9cfe5c33401d90369e4ab56017e5c608db
Dave Luke <dave.luke@gmail.com>
2019-03-28 14:32:59 -0400

    first commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,13 +6,13 @@
 
 // You can delete this file if you're not using it
 
-import './public/static/css/google-font.css';
-import './public/static/css/bootstrap.min.css';
-import './public/static/css/font-awesome.min.css';
-import './public/static/css/animate.css';
-import './public/static/css/customize.css';
+import './static/css/google-font.css';
+import './static/css/bootstrap.min.css';
+import './static/css/font-awesome.min.css';
+import './static/css/animate.css';
+import './static/css/customize.css';
 
-import './public/static/portfolio/css/cubeportfolio.css';
-import './public/static/portfolio/css/portfolio.min.css';
-import "./public/static/css/slick.css";
-import "./public/static/css/slick.theme.css";
+import './static/portfolio/css/cubeportfolio.css';
+import './static/portfolio/css/portfolio.min.css';
+import "./static/css/slick.css";
+import "./static/css/slick.theme.css";
